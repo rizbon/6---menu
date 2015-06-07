c7efe78045864d9b2c3983d7aca63b4f46b58219
rizbon <Evgeniy@iMac.local>
2015-06-07 16:29:47 +0300

    js

--- script/slider.js
+++ script/slider.js
@@ -1,52 +1,31 @@
+$(function(){   
 
-jQuery(document).ready(function ($) {
-
-  $('#checkbox').change(function(){
-    setInterval(function () {
-        moveRight();
-    }, 3000);
-  });
+$('a.control_next').click(function() {
+  
+   $('ol > li:first-child > a').removeClass('active').addClass('control_prev');
+    $('ol > li:last-child > a').removeClass('control_next').addClass('active'); 
+   $('#anim').animate({
     
-    var slideCount = $('#slider ul li').length;
-    var slideWidth = $('#slider ul li').width();
-    var slideHeight = $('#slider ul li').height();
-    var sliderUlWidth = slideCount * slideWidth;
-   
-    $('#slider ul li:last-child').prependTo('#slider ul');
-
-    function moveLeft() {
-        $('ol > li:last-child > a').removeClass('active');
-        $('ol > li:first-child > a').addClass('active');
-        $('#slider ul').animate({
-            left: + slideWidth
-        }, 400, function () {
-            $('#slider ul li:last-child').prependTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    function moveRight() {
-        $('ol > li:first-child > a').removeClass('active');
-        $('ol > li:last-child > a').addClass('active');
-        
-        $('#slider ul').animate({left: - slideWidth}, 400, 
-            function () {
-            $('#slider ul li:first-child').appendTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    $('a.control_prev').click(function () {
-        moveLeft();
-        
-    });
-
-    $('a.control_next').click(function () {
-        moveRight();
+    left: "-920",
     
-        
-    });
+  }, 500, function() {
     
-});    
+});
+});
 
+$('a.control_prev').click(function() {
+   $('ol > li:first-child > a').removeClass('control_prev').addClass('active');
+  $('ol > li:last-child > a').removeClass('active').addClass('control_next'); 
+
+  $('#anim').animate({
+    
+    left: "0",
+    
+  }, 500, function() {
+    
+});
+ 
+});
+
+});
 
